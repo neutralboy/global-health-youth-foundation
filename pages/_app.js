786f68b09d08fa88a15f5ee063c53e7795eb7622
Poornachandra <pc@pop-os.localdomain>
2020-09-20 19:51:10 +0530

    homepage redesign

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,22 @@
 import Head from 'next/head';
 
 import Nav from '../components/Nav';
+import Footer from '../components/Footer';
 
-import "../public/bulma.min.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
       <>
         <Head>
+          <link rel="stylesheet" href="/bulma.min.css" />
           <link rel="icon" href="https://res.cloudinary.com/poorna/image/upload/c_scale,w_16/v1598529986/ghyf/KOSHA_-GHYF.ico" sizes="16x16" /> 
           <link href="https://fonts.googleapis.com/css2?family=Alata&family=Source+Sans+Pro&display=swap" rel="stylesheet" />
         </Head>
         <Nav />
         <div style={{ marginTop: "4rem" }} />
           <Component {...pageProps} />
+        <Footer />
       </>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -6,19 +6,49 @@ export default function Home({ data }) {
       <Head>
         <title>Global Health Youth Foundation | Reimagning Global Heath </title>
       </Head>
-    <section style={{ backgroundImage: "url('https://res.cloudinary.com/poorna/image/upload/c_scale,q_auto:eco,w_1400/v1598456297/edwin-andrade-4V1dC_eoCwg-unsplash.png')", height: "60vh", backgroundRepeat: "no-repeat" }} className="section is-paddingless has-text-centered">
-    <div className="container" style={{ paddingTop: "20vh" }}>
-      <h1 className="is-size-1 family-secondary bg-text"  >Global Health Youth Foundation</h1>
-      <br/><br/>
-      <h2 className="is-size-3 bg-text-black">Re-imagining Global Health</h2>
+    <section style={{ backgroundImage: "url('https://res.cloudinary.com/poorna/image/upload/c_scale,q_auto:eco,w_1300/v1600607663/ghyf/group-of-diverse-youth-hands-joined-EDZ7UNW.png')",  backgroundRepeat: "no-repeat", backgroundSize: "cover" }} className="section is-paddingless has-text-centered">
+    <div className="container" style={{ paddingTop: "8rem", paddingBottom: "8rem" }} >
+
+      <div className="columns is-vcentered">
+        <div className="column is-half-desktop is-full-mobile">
+              <div className="columns is-vcentered">
+                <div className="column is-one-quarter-desktop is-full-mobile">
+                  <img src="https://res.cloudinary.com/poorna/image/upload/c_scale,q_auto:eco,w_50/v1598529986/ghyf/KOSHA_-GHYF.png" alt="logo" />
+                  <h2 className="is-size-2 has-text-white">GHFY</h2>
+                </div>
+                <div className="column is-three-quarters-desktop is-full-mobile">
+                  <h1 className="is-size-1 has-text-primary family-secondary">
+                    Global Health Youth Foundation
+                  </h1>
+                </div>
+              </div>
+        </div>
+        <div className="column is-half-desktop is-full-mobile has-background-grey-lighter">
+          <h3 className="is-size-5">
+              A youth driven think tank which aims to foster youth engagement and provide a platform to Empower Young People to become Global Health Leaders
+          </h3>
+        </div>
+      </div>
+
+
     </div>
     </section>
 
-    <section className="has-background-light section">
-      <div className="has-text-centered">
-        <h3 className="is-size-5">Global Health Youth Foundation is a non-partisan, non- profit, youth run Think Tank to empower Youth to work as future Global Health Leaders</h3>
+    <section className="section">
+      <div className="columns is-multiline is-gapless">
+        {
+          data.map(e=>
+              <div key={Math.random()} className="column has-text-centered is-one-quarter-desktop is-full-mobile">
+                <div className="box same-height" style={{ backgroundColor: e.color }} >
+                  <img src={e.img} />
+                  <p style={{ paddingTop: "2rem" }} className="is-size-4 has-text-light">{e.title}</p>
+                </div>
+              </div>
+            )
+        }
       </div>
     </section>
+
 
       <section className="section">
         <div className="container">
@@ -28,36 +58,39 @@ export default function Home({ data }) {
             </h2>
           </div>
           <br/>
-            <p className="is-size-5">
-            GHYF aims to form a youth-driven Think Tank to engage youth in the policy making process and inspire to lead a platform for students and young professionals in the field of global health to engage, network and build capacity through innovative ideas, professional development and discussions to promote youth to enact tangible changes, in improving global health and achieving health equity by fostering quality and ethical research, education, advocacy, leadership, and community action, while building a peer and mentorship network in the process.
-            </p><br/>
-        </div>
-      </section>
-      <section className="section has-background-primary">
-        <div className="container">
-        <h2 className="has-text-white is-size-2 is-family-secondary">Values.</h2><br/>
-        <div className="columns is-multiline">
-          {
-            data.map(e=>
-                <div key={Math.random()} className="column has-text-centered is-half-desktop is-full-mobile">
-                  <div className=" box "><p className="is-size-4">{e}</p></div>
-                </div>
-              )
-          }
-        </div>
-        </div>
-      </section>
+            <div className="columns is-vcentered">
+              <div className="column is-half-desktop is-full-mobile">
+                <p className="is-size-5">
+                    To form a youth-driven Think Tank to engage youth in the policy making process and inspire to lead a platform for students and young professionals in the field of Global Health to engage, network and build capacity through innovative ideas, professional development and discussions
+                </p><br/>
+              </div>
+              <div className="column is-half-desktop is-full-mobile">
+                <img alt="youth driven" src="https://res.cloudinary.com/poorna/image/upload/c_scale,q_auto:eco,w_600/v1600608690/ghyf/yingchou-han-IJrIeCs3D4g-unsplash.png" />
+              </div>
+            </div>
+            <div className="columns is-vcentered">
+              <div className="column is-half-desktop is-full-mobile">
+                <img alt="youth driven" src="https://res.cloudinary.com/poorna/image/upload/c_scale,q_auto:eco,w_600/v1600608994/ghyf/hannah-olinger-NXiIVnzBwZ8-unsplash.png" />
+              </div>
+              <div className="column is-half-desktop is-full-mobile">
+                <p className="is-size-5">
+                To Promote youth to enact Tangible changes, in improving Global Health and achieving Health Equity by fostering quality and ethical research, education, advocacy, leadership, and community action, while building a peer and mentorship network in the process
+              </p><br/>
+              </div>
+            </div>
 
+        </div>
+      </section>
 
     </>
   )
 }
 export async function getStaticProps() {
   let data = [
-    "Enhance Critical Thinking",
-    "Youth Dialogue and Partnership",
-    "Health and Equity",
-    "Sustainability, Ideation and Innovation"
+    { title: "Enhance Critical Thinking", img: "https://img.icons8.com/dusk/64/000000/blunt-bulb.png", color: "#ba0f26"},
+    {title: "Youth Dialogue and Partnership", img: "https://img.icons8.com/ultraviolet/80/000000/user-group-woman-woman.png", color: "#38b6ff"},
+    {title: "Health and Equity", img: "https://img.icons8.com/dusk/64/000000/mental-health.png", color: "#008037"},
+    {title: "Sustainability, Ideation and Innovation", img: "https://img.icons8.com/dusk/64/000000/recycle-sign.png", color: "#ffde59"}
   ]
   return {
     props: {
